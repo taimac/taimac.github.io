a0805a31f3a76161f77aae35366953d0ee1dfec7
Tailor Maciel <45276175+taimac@users.noreply.github.com>
2019-11-14 01:47:29 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.jpg') {
-      myImage.setAttribute ('src','images/firefox2.jpg');
+    if(mySrc === 'images/test1.jpg') {
+      myImage.setAttribute ('src','images/test2.jpg');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.jpg');
+      myImage.setAttribute ('src','images/test1.jpg');
     }
 }
 
